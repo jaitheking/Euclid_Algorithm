4ac5148990e449aa6308c7bcf0ad37f38e58b138
jaitheking <jaitheking4858@gmail.com>
2015-01-24 15:54:16 +0800

    Euclid.cpp added.

---
+++ euclid.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Euclid`s Algorithm
+int euclid(int a ,int b){
+	int n1,n2,r;
+    n1= a;
+    n2 = b;
+	while(n2 > 0){
+		r= n1%n2;
+		n1=n2;
+		n2=r;
+
+	}
+	cout<<n1<<" is the proof of GCD"<<endl;
+
+	return n1;
+}
+
+int extend_euclid(int a,int b){
+    int a1,a2,a3,b1,b2,b3,q;
+    int t1,t2,t3;
+    a1 = 1;
+    a2 = 0;
+    a3 = a;
+    b1 = 0;
+    b2 = 1;
+    b3 = b;
+
+    while(b3 >=1){
+
+    if (b3==0){
+        cout<<"No inverse."<<endl;
+    	return a3 = euclid(a,b);
+
+    	}
+    if (b3==1){
+    	cout<<b3<<"and "<<b2<<" is the inverse ."<<endl;
+    	return b3 = euclid(a,b) && b2;
+
+    	}
+    q = abs(a3/b3);
+    t1 = a1-(q*b1);
+ 	t2 = a2-(q*b2);
+ 	t3 = a3-(q*b3);
+ 	a1 = b1;
+ 	a2 = b2;
+ 	a3 = b3;
+ 	b1 = t1;
+ 	b2 = t2;
+ 	b3 = t3;
+    cout<<b3<<"...Calculating.."<<endl;
+ 	};
+
+
+}
+
+int main(){
+    int a,b;
+	cout<<"Please enter two integers:";
+	cin>>a>>b;
+	//euclid(a,b);
+ 	extend_euclid(a,b);
+
+
+return 0;
+}
